e3cb6622a9bf142569e05efeeffe766cfa8a8a03
2amkoding <2amkoding@gmail.com>
2025-06-13 21:33:45 -0500

    update DB to SqlLite. Confirm website works w security risk

---
+++ Debuggingc#/02-debuggingDataAndInputs/Samples.Debugging.Web/Samples.Debugging.Web.WebUI/Migrations/20250614022922_InitialCreate.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Samples.Debugging.Web.WebUI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExpenseTypeCategories",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpenseTypeCategories", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExpenseTypes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    CategoryId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpenseTypes", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_ExpenseTypes_ExpenseTypeCategories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "ExpenseTypeCategories",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Expenses",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    DateIncurred = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Location = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<double>(type: "decimal(18,2)", nullable: false),
+                    ExpenseTypeID = table.Column<int>(type: "INTEGER", nullable: false),
+                    UserID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Expenses", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Expenses_ExpenseTypes_ExpenseTypeID",
+                        column: x => x.ExpenseTypeID,
+                        principalTable: "ExpenseTypes",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Expenses_ExpenseTypeID",
+                table: "Expenses",
+                column: "ExpenseTypeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExpenseTypes_CategoryId",
+                table: "ExpenseTypes",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Expenses");
+
+            migrationBuilder.DropTable(
+                name: "ExpenseTypes");
+
+            migrationBuilder.DropTable(
+                name: "ExpenseTypeCategories");
+        }
+    }
+}
